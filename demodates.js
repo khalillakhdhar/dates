e945c3a2a6df45bd36b83f56e04511e4ce6fe47c
khalil <khalillakhdharatc@gmail.com>
2022-06-14 08:56:26 +0100

    Initial commit

---
+++ demodates.js
@@ -0,0 +1,8 @@
+var dt=new Date();
+console.log("full date",dt);
+console.log("year", dt.getFullYear());
+console.log("month",new Date().getMonth()+1);
+console.log("day",new Date().getDate());
+console.log("day order",dt.getDay());
+// ecrire un programme permettant d'afficher le mois et le jour en texte 
+// a partir de la date du jour
